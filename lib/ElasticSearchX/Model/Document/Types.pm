a0ef1474bad9cb07ddef722ad0ef348454893c2a
Moritz Onken <onken@netcubed.de>
2011-07-09 19:44:47 +0200

    moved stuff to MooseX::Types::ElasticSearch

--- lib/ElasticSearchX/Model/Document/Types.pm
+++ lib/ElasticSearchX/Model/Document/Types.pm
@@ -7,44 +7,17 @@ use MooseX::Attribute::Deflator;
 use DateTime;
 use JSON;
 use Scalar::Util qw(blessed);
+use MooseX::Types::ElasticSearch qw(:all);
 
 use MooseX::Types -declare => [
     qw(
-      Location
-      QueryType
-      ES
       Type
       ) ];
 
+use Sub::Exporter -setup => { exports => [qw(Location QueryType ES Type)] };
+
 use MooseX::Types::Moose qw/Int Str ArrayRef HashRef/;
 use MooseX::Types::Structured qw(Dict Tuple Optional);
-
-coerce ArrayRef, from Str, via { [$_] };
-
-class_type ES, { class => 'ElasticSearch' };
-coerce ES, from Str, via {
-    my $server = $_;
-    $server = "127.0.0.1$server" if ( $server =~ /^:/ );
-    return
-      ElasticSearch->new( servers   => $server,
-                          transport => 'httptiny',
-                          timeout   => 30, );
-};
-
-coerce ES, from HashRef, via {
-    return ElasticSearch->new(%$_);
-};
-
-coerce ES, from ArrayRef, via {
-    my @servers = @$_;
-    @servers = map { /^:/ ? "127.0.0.1$_" : $_ } @servers;
-    return
-      ElasticSearch->new( servers   => \@servers,
-                          transport => 'httptiny',
-                          timeout   => 30, );
-};
-
-enum QueryType, qw(query_and_fetch query_then_fetch dfs_query_and_fetch dfs_query_then_fetch);
 
 class_type 'DateTime';
 coerce 'DateTime', from Str, via {
@@ -68,19 +41,9 @@ $REGISTRY->add_type_constraint(
 
 Moose::Util::TypeConstraints::add_parameterizable_type($REGISTRY->get_type_constraint(Type));
 
-
-subtype Location,
-  as ArrayRef,
-  where { @$_ == 2 },
-  message { "Location is an arrayref of longitude and latitude" };
-
-coerce Location, from HashRef,
-  via { [ $_->{lon} || $_->{longitude}, $_->{lat} || $_->{latitude} ] };
-coerce Location, from Str, via { [ reverse split(/,/) ] };
-
 use MooseX::Attribute::Deflator;
 deflate 'Bool', via { \($_ ? 1 : 0) };
-inflate 'Bool', via { warn "FOO"; $_ ? 1 : 0 };
+inflate 'Bool', via { $_ ? 1 : 0 };
 my @stat =
   qw(dev ino mode nlink uid gid rdev size atime mtime ctime blksize blocks);
 deflate 'File::stat', via { return { List::MoreUtils::mesh( @stat, @$_ ) } };
@@ -93,6 +56,7 @@ deflate Location, via { [ $_->[0] + 0, $_->[1] + 0 ] };
 deflate Type . '[]', via { ref $_ eq 'HASH' ? $_ : $_->meta->get_data($_) };
 deflate 'ArrayRef[]', via {
     my ($attr, $constraint, $deflate) = @_;
+    return $_ if($attr->dynamic);
     $constraint = $constraint->parent
         if(ref $constraint eq 'MooseX::Types::TypeDecorator');
     my $value = [@$_];

b5a436f878855cd574955d0e0f60f8ca5a1f6a38
Moritz Onken <onken@netcubed.de>
2011-08-18 14:50:06 +0200

    fixed aliases

--- lib/ElasticSearchX/Model/Role.pm
+++ lib/ElasticSearchX/Model/Role.pm
@@ -8,9 +8,11 @@ use ElasticSearchX::Model::Document::Types qw(ES);
 has es => ( is => 'rw', lazy_build => 1, coerce => 1, isa => ES );
 
 sub _build_es {
-    ElasticSearch->new( servers   => '127.0.0.1:9200',
-                        transport => 'http',
-                        timeout   => 30, );
+    ElasticSearch->new(
+        servers   => '127.0.0.1:9200',
+        transport => 'http',
+        timeout   => 30,
+    );
 }
 
 sub deploy {
@@ -18,33 +20,44 @@ sub deploy {
     my $t = $self->es->transport;
     foreach my $name ( $self->meta->get_index_list ) {
         my $index = $self->index($name);
-        next if($index->alias_for && $name eq $index->alias_for);
-        $name = $index->alias_for if($index->alias_for);
+        next if ( $index->alias_for && $name eq $index->alias_for );
+        $name = $index->alias_for if ( $index->alias_for );
         try { $t->request( { method => 'DELETE', cmd => "/$name", } ); }
-            if($params{delete});
+        if ( $params{delete} );
         my $dep     = $index->deployment_statement;
         my $mapping = delete $dep->{mappings};
         try {
             $t->request(
-                     { method => 'PUT',
-                       cmd    => "/$name",
-                       data   => $dep,
-                     } );
+                {   method => 'PUT',
+                    cmd    => "/$name",
+                    data   => $dep,
+                }
+            );
         };
         sleep(1);
-        while(my($k,$v) = each %$mapping) {
-              $t->request(
-                           { method => 'PUT',
-                             cmd    => "/$name/$k/_mapping",
-                             data   => { $k => $v },
-                           } );
+        while ( my ( $k, $v ) = each %$mapping ) {
+            $t->request(
+                {   method => 'PUT',
+                    cmd    => "/$name/$k/_mapping",
+                    data   => { $k => $v },
+                }
+            );
         }
-        if(my $alias = $index->alias_for) {
-            my $aliases = $self->es->get_aliases(index => 'cpan')->{aliases}->{cpan};
-            $self->es->aliases( actions => [
-                (map { { remove => { index => $_, alias => $index->name } } } @$aliases ),
-                { add => { index => $name, alias => $index->name } }
-            ] );
+        if ( my $alias = $index->alias_for ) {
+            my $aliases
+                = $self->es->get_aliases( index => $index->name )->{aliases}
+                ->{ $index->name };
+            my $actions = [
+                (   map {
+                        { remove => { index => $_, alias => $index->name }
+                        }
+                        } @$aliases
+                ),
+                { add => { index => $alias, alias => $index->name } }
+            ];
+            $self->es->aliases(
+                actions => $actions
+            );
         }
     }
     return 1;
@@ -52,7 +65,7 @@ sub deploy {
 
 sub bulk {
     my $self = shift;
-    return ElasticSearchX::Model::Bulk->new(es => $self->es, @_);
+    return ElasticSearchX::Model::Bulk->new( es => $self->es, @_ );
 }
 
 1;

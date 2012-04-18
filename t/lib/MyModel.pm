48c7bbee7d82bbbbd3cdbd23f2106bce461785c0
Moritz Onken <onken@netcubed.de>
2012-04-18 23:17:51 +0200

    require ES 0.19.2

--- t/lib/MyModel.pm
+++ t/lib/MyModel.pm
@@ -3,16 +3,25 @@ use Moose;
 use Test::More;
 use IO::Socket::INET;
 use ElasticSearchX::Model;
+use version;
 
 index twitter => ( namespace => 'MyModel' );
 
 sub testing {
     my $class = shift;
     unless ( IO::Socket::INET->new('127.0.0.1:9900') ) {
-        plan skip_all => 'Requires an ElasticSearch server running on port 9900';
+        plan skip_all =>
+            'Requires an ElasticSearch server running on port 9900';
     }
 
     my $model = $class->new( es => ':9900' );
+    my $version
+        = version->parse( $model->es->current_server_version->{number} )
+        ->numify;
+    if ( $version < 0.019002 ) {
+        plan skip_all => 'Requires ElasticSearch 0.19.2';
+    }
+
     # $model->es->trace_calls(1);
     ok( $model->deploy( delete => 1 ), 'Deploy ok' );
     return $model;
